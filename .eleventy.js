b78729c3138e1ad0ec79cce7b919c1c86d11d76c
alex <noodlecontract@gmail.com>
2022-11-03 08:38:29 -0400

    add last updated timestamp

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const { DateTime } = require("luxon")
+
 module.exports = function(eleventyConfig) {
   // Output directory: _site
   eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addFilter("formatDate", (d) => {
+    return DateTime.fromJSDate(d).toUTC().toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     pathPrefix: "/monkey-see/"
