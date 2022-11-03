9eaee047ffb516186df4c984d484c6174ec9ee4a
alex <noodlecontract@gmail.com>
2022-11-02 20:20:18 -0400

    tweak pull script, moar pages

--- pull.sh
+++ pull.sh
@@ -7,6 +7,7 @@ IFS=$'\n'
 
 # TODO: replace, calc max based on stuff currently in ouput dir
 max=${1:-20}
+# # assume that between runs there have been a max of 100 new mints
 # max=`bc <<< "`ls output_raw/artifacts | cut -f 1 -d '.' | sort -n | tail -n1` + 100"`
 
 mkdir -p output_raw/artifacts
@@ -18,10 +19,16 @@ seq 1 $max | xargs -P 4 -I {} sh -c "(test ! -e output_raw/artifacts/{}.json) &&
 invalid=`{ grep -rL 'name' output_raw/artifacts || true; }`;
 [[ ! -z $invalid ]] && (xargs rm <<< $invalid)
 
+# TODO:
+# - -c below
+# - prob prune unneeded attrs
+# - prob make all_artifacts a projection of artifacts_and_witches to get e.g. witch name
+
 # wombo combo join witches + artifacts by witch id
-jq -cs '.[0] * .[1]' \
-  <(jq 'group_by(.id) | map({key: .[0].id|tostring, value: {witch: .[0]}}) | from_entries' output_raw/witches.json) \
-  <(jq 'select(.coven.witchId != null)' output_raw/artifacts/* | jq -s | jq 'group_by(.coven.witchId) | map({key: .[0].coven.witchId|tostring, value: {artifacts: .}}) | from_entries') \
+jq -s '.[0] * .[1] | values[] | select(.artifacts != null)' \
+    <(jq 'group_by(.id) | map({key: .[0].id|tostring, value: {witch: .[0]}}) | from_entries' output_raw/witches.json) \
+    <(jq 'select(.coven.witchId != null)' output_raw/artifacts/* | jq -s | jq 'group_by(.coven.witchId) | map({key: .[0].coven.witchId|tostring, value: {artifacts: .}}) | from_entries') \
+  | jq -s \
   > _data/artifacts_and_witches.json
 
-jq -cs '.' output_raw/artifacts/* > _data/all_artifacts.json
+jq -s '.' output_raw/artifacts/* > _data/all_artifacts.json
