15a144bc865326564028fe705059db26c5c47f59
alex <noodlecontract@gmail.com>
2022-11-17 07:15:56 -0500

    quick cache busting

--- .eleventy.js
+++ .eleventy.js
@@ -3,8 +3,16 @@ const { DateTime } = require("luxon");
 module.exports = function(eleventyConfig) {
   // Output directory: _site
   eleventyConfig.addPassthroughCopy("assets");
+
   eleventyConfig.addFilter("formatDate", (d) => {
     return DateTime.fromJSDate(d).toUTC().toLocaleString(DateTime.DATE_MED);
+  });
+
+  eleventyConfig.addFilter("cacheBust", (url) => {
+    const [urlPart, paramPart] = url.split("?");
+    const params = new URLSearchParams(paramPart || "");
+    params.set("v", DateTime.local().toFormat("X"));
+    return `${urlPart}?${params}`;
   });
 
   eleventyConfig.addLiquidShortcode("formatAttunementMods", (mods) => {
